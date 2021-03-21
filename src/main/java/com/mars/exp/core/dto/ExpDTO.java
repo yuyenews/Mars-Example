ba1835624d003724e6c688cee01b1723df289c86
yuyenews <631941527@qq.com>
2021-03-21 14:03:58 +0800

    依赖包升级到 3.2.18

--- src/main/java/com/mars/exp/core/dto/ExpDTO.java
+++ src/main/java/com/mars/exp/core/dto/ExpDTO.java
@@ -18,7 +18,7 @@ public class ExpDTO {
      * 1. 这里的字段名必须跟查询语句中的字段名 一模一样（如果取了别名就跟别名一样）
      * 2. 如果是增删改操作，并且你用的是框架自带的方法，那么必须跟数据表的字段名一模一样
      * 3. 如果数据库是user_name 那么这里也必须是user_name，不可以是userName
-     * 如果加了JSONField注解，那么只要保证name属性跟数据库字段名一样即可
+     * 如果加了JsonProperty注解，那么只要保证value属性跟数据库字段名一样即可
      */
     @JsonProperty(value = "数据库字段名")
     private Integer id;
